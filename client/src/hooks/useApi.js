a4d06ae835c5ae0de5e08bc57fc93bde5f7e2f96
lokeshk2492 <lokeshk2492@gmail.com>
2026-05-20 12:04:04 +0530

    api changes

--- client/src/hooks/useApi.js
+++ client/src/hooks/useApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.PROD 
-  ? (import.meta.env.VITE_API_URL || '') + '/api' 
+const API_BASE_URL = import.meta.env.VITE_API_URL 
+  ? import.meta.env.VITE_API_URL + '/api' 
   : '/api';
 
 const api = axios.create({
